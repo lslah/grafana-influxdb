f04213adef67064ffda73fc26a5f5c9408707c75
Thomas Wienecke <wienecke.t@gmail.com>
2017-07-22 19:46:10 +0200

    Basic grafana-influxdb setup

---
+++ gathering/mem.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+# A little script to send test data to an influxdb installation
+# Attention, the non-core library 'requests' is used. You'll need to install it first:
+# http://docs.python-requests.org/en/master/user/install/
+
+import json
+import math
+import requests
+import sys
+import psutil
+import subprocess
+from threading import Timer
+
+IP = "influxdb"        # The IP of the machine hosting your influxdb instance
+DB = "wadus"               # The database to write to, has to exist
+USER = "admin"             # The influxdb user to authenticate with
+PASSWORD = "admin"  # The password of that user
+TIME = 0.1                  # Delay in seconds between two consecutive updates
+
+def send(data):
+    r = requests.post("http://%s:8086/write?db=%s" %(IP, DB), auth=(USER, PASSWORD), data=data)
+    if r.status_code != 204:
+        print("Failed to add point to influxdb (%d) - aborting." % r.status_code)
+        sys.exit(1)
+
+def schedule(time, cmd):
+    def repeat():
+        Timer(time, repeat).start()
+        cmd()
+    repeat()
+
+def main():
+    schedule(1.0, lambda: send('cpu_percent value=%s' % psutil.cpu_percent()))
+    schedule(1.0, lambda: send('mem_available value=%s' % psutil.virtual_memory().available))
+    schedule(1.0, lambda: send('google.ping_time value=%s' % ping_google()))
+
+def ping_google():
+    ping_result = subprocess.check_output(['ping', '-c', '1', 'google.com']).decode()
+    return ping_result.split('\n')[4].split('/')[4]
+
+if __name__ == '__main__':
+    main()
+
+#while True:
+#    send('cpu_percent value=%s' % psutil.cpu_percent())
+#    send('mem_available value=%s' % psutil.virtual_memory().available)
+#    ping_result = subprocess.check_output(['ping', '-c', '1', 'google.com']).decode()
+#    ping_time = ping_result.split('\n')[4].split('/')[4]
+#    send('google.ping_time value=%s' % ping_time)
+#    sleep(TIME)
